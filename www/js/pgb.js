b7240ff041b57c6753aae39e120a053a4a68347e
simon177 <szymon.banowski@gmail.com>
2017-04-10 21:46:33 +0200

    first commit

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -4,17 +4,16 @@ function init() {
 
 function onDeviceReady() {
 	navigator.notification.beep(2);
-	deviceInfo();
+	Info();
 }
 
-function deviceInfo() {
+function Info() {
 
-	info =  'Device Model   : '    + device.model + '<br>' + 
-			'Device Name    : '     + device.name + '<br>' + 
-			'Device Cordova : '  + device.cordova + '<br>' + 
-			'Device Platform: ' + device.platform + '<br>' + 
-			'Device UUID    : '     + device.uuid + '<br>' + 
-			'Device Version : '  + device.version + '<br>';
+	info =  'The graphical user interface (GUI), is a type of user interface that allows users to interact with electronic
+devices through graphical icons and visual indicators such as secondary notation, instead of text-based user
+interfaces, typed command labels or text navigation. GUIs were introduced in reaction to the perceived steep
+learning curve of command-line interfaces (CLIs), which require commands to be typed on a computer
+keyboard.';
 
 	document.getElementById("deviceDetails").innerHTML = info;	
 }
