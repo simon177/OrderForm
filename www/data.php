dd920398a77d1c46800f9012bd0caf1a1c3fc838
michalcz <michal@grodzisko.com>
2017-05-16 20:34:43 +0200

    API & PHP

---
+++ www/data.php
@@ -0,0 +1,21 @@
+<?php
+$APIkey='fe753bc888bb6a75490621597fc3f4f93184ef9a2ffb5cffab3be87813b7023f';
+$country_id = 169;
+$date = $_POST['date'];
+$league = $_POST['league'];
+
+$curl_options = array(
+	CURLOPT_URL => "https://apifootball.com/api/?action=get_events&from=".$date."&to=".$date."&league_id=".$league."&APIkey=$APIkey",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_HEADER => false,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_CONNECTTIMEOUT => 5
+);                              
+ 
+$curl = curl_init();
+curl_setopt_array( $curl, $curl_options );
+$result = curl_exec( $curl );
+
+$result = json_encode($result);
+    
+echo $result;
